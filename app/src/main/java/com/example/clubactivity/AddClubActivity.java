9d45f5c292ecfe40230ffe2613f5fbdf8c4ec2f8
cme10575 <cme10575@gmail.com>
2019-07-19 19:09:05 +0900

    갤러리 이미지 가져오기

--- app/src/main/java/com/example/clubactivity/AddClubActivity.java
+++ app/src/main/java/com/example/clubactivity/AddClubActivity.java
@@ -2,24 +2,63 @@ package com.example.clubactivity;
 
 //import android.support.v7.app.AppCompatActivity;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class AddClubActivity extends AppCompatActivity {
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
-   private SharedPreferences preferences;
+public class AddClubActivity extends AppCompatActivity {
+    ImageView need_Photo;
+    Spinner camp_kind;
+    EditText need_thing;
+    EditText lettable_thing;
+
+    private SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_club);
+
+        Button button = findViewById(R.id.select_img_btn);
+        button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+
+                startActivityForResult(Intent.createChooser(intent, "이미지를 선택하세요"), 1000);
+
+            }
+        });
+
     }
 
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(resultCode == RESULT_OK && requestCode == 1){
+            ImageView imageView = findViewById(R.id.user_image);
+            try{
+                InputStream inputStream = getContentResolver().openInputStream(data.getData());
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                imageView.setImageBitmap(bitmap);
+            }
+            catch(FileNotFoundException e){
+                e.printStackTrace();
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/clubactivity/ClubFragment.java
+++ app/src/main/java/com/example/clubactivity/ClubFragment.java
@@ -90,4 +90,8 @@ public class ClubFragment extends Fragment {
 
         }
     }
+
+
+
+
 }
