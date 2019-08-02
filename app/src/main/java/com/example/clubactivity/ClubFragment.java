62b9b82b1ad5af5adbd8de1f7d8e6a14646a9bf0
cme10575 <cme10575@gmail.com>
2019-08-02 13:09:01 +0900

    문화행사API 코드 추가했으나 오류로 주석처리 해놓음

--- app/src/main/java/com/example/clubactivity/ClubFragment.java
+++ app/src/main/java/com/example/clubactivity/ClubFragment.java
@@ -1,5 +1,6 @@
 package com.example.clubactivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
@@ -12,7 +13,10 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TabHost;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -30,7 +34,8 @@ import java.util.Locale;
 public class ClubFragment extends Fragment {
     private View view;
     FloatingActionButton fab;
-    ListView listview;
+    ListView myClub_Listview;
+    ListView whole_ClubListView;
     ChatViewAdapter myClub_adapter;
     ChatViewAdapter searchClub_adapter;
     ChatViewAdapter wholeClub_adapter;
@@ -40,32 +45,49 @@ public class ClubFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.club, null);
+
+        TabHost tabHost1 = (TabHost) view.findViewById(R.id.tapHost_chatlist) ;
+        tabHost1.setup() ;
+
+        // 첫 번째 Tab. (탭 표시 텍스트:"TAB 1"), (페이지 뷰:"content1")
+        TabHost.TabSpec ts1 = tabHost1.newTabSpec("Tab Spec 1") ;
+        ts1.setContent(R.id.content1_chatlist) ;
+        ts1.setIndicator("전체 동호회") ;
+        tabHost1.addTab(ts1)  ;
+
+        // 두 번째 Tab. (탭 표시 텍스트:"TAB 2"), (페이지 뷰:"content2")
+        TabHost.TabSpec ts2 = tabHost1.newTabSpec("Tab Spec 2") ;
+        ts2.setContent(R.id.content2_chatlist) ;
+        ts2.setIndicator("나의 동호회") ;
+        tabHost1.addTab(ts2) ;
 
         // Adapter 생성
         myClub_adapter = new ChatViewAdapter() ;
         searchClub_adapter = new ChatViewAdapter();
         wholeClub_adapter = new ChatViewAdapter();
 
-        view = inflater.inflate(R.layout.club, null);
         // 리스트뷰 참조 및 Adapter달기
-        listview = (ListView) view.findViewById(R.id.listview);
-        listview.setAdapter(myClub_adapter);
+        myClub_Listview = (ListView) view.findViewById(R.id.myclub_listview);
+        myClub_Listview.setAdapter(myClub_adapter);
+        whole_ClubListView = (ListView) view.findViewById(R.id.wholeclub_listview);
+        whole_ClubListView.setAdapter(wholeClub_adapter);
 
-        // 첫 번째 아이템 추가.
-        myClub_adapter.addItem(ContextCompat.getDrawable(getActivity(), R.drawable.cat_dog),
+        // 임시 아이템 추가.
+        wholeClub_adapter.addItem(ContextCompat.getDrawable(getActivity(), R.drawable.cat_dog),
                 "일러스트 동호회 모집", "일러스트에 관심 있으신 분들 환영합니다~~") ;
-        // 두 번째 아이템 추가.
-        myClub_adapter.addItem(ContextCompat.getDrawable(getActivity(), R.drawable.ic_home_black_24dp),
+        wholeClub_adapter.addItem(ContextCompat.getDrawable(getActivity(), R.drawable.ic_home_black_24dp),
                 "Circle", "Account Circle Black 36dp") ;
-        // 세 번째 아이템 추가.
-        myClub_adapter.addItem(ContextCompat.getDrawable(getActivity(), R.drawable.ic_people_black_24dp),
-                "Ind", "Assignment Ind Black 36dp") ;
-
         wholeClub_adapter.addItem(ContextCompat.getDrawable(getActivity(), R.drawable.ic_people_black_24dp),
                 "Ind", "Assignment Ind Black 36dp");
+        myClub_adapter.addItem(ContextCompat.getDrawable(getActivity(), R.drawable.ic_people_black_24dp),
+                "Ind", "Assignment Ind Black 36dp") ;
+        myClub_adapter.addItem(ContextCompat.getDrawable(getActivity(), R.drawable.ic_home_black_24dp),
+                "Circle", "Account Circle Black 36dp") ;
 
 
-        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        //아이템 클릭시 채팅방 들어가기
+        myClub_Listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Toast.makeText(getActivity() , ((ChatViewItem)myClub_adapter.getItem(i)).getTitle(),Toast.LENGTH_LONG).show();
@@ -93,11 +115,11 @@ public class ClubFragment extends Fragment {
             public void afterTextChanged(Editable edit) {
                 String text = searchBar.getText().toString().toLowerCase(Locale.getDefault());
                 //내가 소속되지 않은 동호회 리스트 검색
-                listview.setAdapter(searchClub_adapter);
+                whole_ClubListView.setAdapter(searchClub_adapter);
                 searchClub_adapter.removeAllItem();
 
                 if (searchBar.length() == 0) {
-                    listview.setAdapter(myClub_adapter);
+                    whole_ClubListView.setAdapter(wholeClub_adapter);
                     // 문자 입력이 없을때는 모든 데이터를 보여준다.
                     //for (ChatViewItem mItem : wholeClub_adapter.chatViewItemList) {
                     //    searchClub_adapter.addItem(mItem);
@@ -139,6 +161,7 @@ public class ClubFragment extends Fragment {
             }
         });
         return view;
+
     }
 
 
@@ -155,9 +178,6 @@ public class ClubFragment extends Fragment {
             myClub_adapter.notifyDataSetChanged();
         }
     }
-
-
-
 
 
 }
--- app/src/main/java/com/example/clubactivity/HomeFragment.java
+++ app/src/main/java/com/example/clubactivity/HomeFragment.java
@@ -1,7 +1,9 @@
 package com.example.clubactivity;
 
+import android.animation.ArgbEvaluator;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,15 +13,23 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
+import com.github.jhonnyx2012.horizontalpicker.DatePickerListener;
+import com.github.jhonnyx2012.horizontalpicker.HorizontalPicker;
 import com.smarteist.autoimageslider.IndicatorAnimations;
 import com.smarteist.autoimageslider.IndicatorView.draw.controller.DrawController;
 import com.smarteist.autoimageslider.SliderAnimations;
 import com.smarteist.autoimageslider.SliderView;
+
+import org.joda.time.DateTime;
+
+import java.util.ArrayList;
 import java.util.Date;
 import java.text.SimpleDateFormat;
+import java.util.List;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements DatePickerListener {
 
     private View view;
     SliderView sliderView;
@@ -28,21 +38,36 @@ public class HomeFragment extends Fragment {
     SimpleDateFormat mFormat = new SimpleDateFormat("MM/dd"); //날짜형식
     TextView mTextView;
 
+    ViewPager viewPager;
+    Adapter adapter;
+    List<Model> models;
+    //Integer[] colors = null;
+    ArgbEvaluator argbEvaluator = new ArgbEvaluator();
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.home, container, false);
 
+
+
+        //데이트영역
+        // find the picker
+        HorizontalPicker picker = (HorizontalPicker) view.findViewById(R.id.datePicker);
+        // initialize it and attach a listener
+        picker
+                .setListener(this)
+                .init();
+        picker.setBackgroundColor(Color.WHITE);
+        picker.setDate(new DateTime());
+
+
+        //슬라이드영역
         sliderView = view.findViewById(R.id.imageSlider);
-        mTextView = view.findViewById(R.id.monday_day);
-        mTextView.setText(getTime()); // 날짜가져오기
-        mTextView = view.findViewById(R.id.tuesday_day);
-        mTextView.setText(getTime()+1); // 날짜가져오기
+        final SliderAdapter sliderAdapter = new SliderAdapter(getActivity());
+        sliderAdapter.setCount(5); //카드뷰 개수 정해주기
 
-        final SliderAdapter adapter = new SliderAdapter(getActivity());
-        adapter.setCount(5); //카드뷰 개수 정해주기
-
-        sliderView.setSliderAdapter(adapter);
+        sliderView.setSliderAdapter(sliderAdapter);
 
         sliderView.setIndicatorAnimation(IndicatorAnimations.SLIDE);
         sliderView.setSliderTransformAnimation(SliderAnimations.CUBEINROTATIONTRANSFORMATION);
@@ -55,6 +80,62 @@ public class HomeFragment extends Fragment {
             @Override
             public void onIndicatorClicked(int position) {
                 sliderView.setCurrentPagePosition(position);
+            }
+        });
+
+
+
+
+        //밑에 뷰페이저 영역
+        models = new ArrayList<>();
+        models.add(new Model(R.drawable.seoul_is_learning, "Brochure", "Brochure is an informative paper document (often also used for advertising) that can be folded into a template"));
+        models.add(new Model(R.drawable.cat_dog, "Sticker", "Sticker is a type of label: a piece of printed paper, plastic, vinyl, or other material with pressure sensitive adhesive on one side"));
+        models.add(new Model(R.drawable.seoul_is_learning, "Poster", "Poster is any piece of printed paper designed to be attached to a wall or vertical surface."));
+        models.add(new Model(R.drawable.seoul_is_learning, "Namecard", "Business cards are cards bearing business information about a company or individual."));
+
+        adapter = new Adapter(models, getActivity());
+
+        viewPager = view.findViewById(R.id.viewPager);
+        viewPager.setAdapter(adapter);
+        viewPager.setPadding(130, 0, 130, 0);
+
+     /*   Integer[] colors_temp = {
+                getResources().getColor(R.color.white),
+                getResources().getColor(R.color.black),
+                getResources().getColor(R.color.white),
+                getResources().getColor(R.color.black)
+        };
+
+        colors = colors_temp;*/
+
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+                /*if (position < (adapter.getCount() -1) && position < (colors.length - 1)) {
+                    viewPager.setBackgroundColor(
+
+                            (Integer) argbEvaluator.evaluate(
+                                    positionOffset,
+                                    colors[position],
+                                    colors[position + 1]
+                            )
+                    );
+                }
+
+                else {
+                    viewPager.setBackgroundColor(colors[colors.length - 1]);
+                }*/
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
             }
         });
 
@@ -74,4 +155,8 @@ public class HomeFragment extends Fragment {
     }
 
 
+    @Override
+    public void onDateSelected(DateTime dateSelected) {
+        Log.i("HorizontalPicker", "Selected date is " + dateSelected.toString());
+    }
 }
