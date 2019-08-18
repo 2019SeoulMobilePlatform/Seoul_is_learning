9eab262ad1b52c1cea26361ebfd666666efa15e5
yeop96 <ekdh787@gmail.com>
2019-08-18 21:28:24 +0900

    종료 팝업

--- app/src/main/java/com/example/clubactivity/MainActivity.java
+++ app/src/main/java/com/example/clubactivity/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.clubactivity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.MenuItem;
 
@@ -144,7 +146,28 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }*/
-        finish();
+        // AlertDialog 빌더를 이용해 종료시 발생시킬 창을 띄운다
+        AlertDialog.Builder alBuilder = new AlertDialog.Builder(this);
+        alBuilder.setMessage("종료하시겠습니까?");
+
+        // "예" 버튼을 누르면 실행되는 리스너
+        alBuilder.setPositiveButton("예", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish(); // 현재 액티비티를 종료한다. (MainActivity에서 작동하기 때문에 애플리케이션을 종료한다.)
+            }
+        });
+        // "아니오" 버튼을 누르면 실행되는 리스너
+        alBuilder.setNegativeButton("아니오", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                return; // 아무런 작업도 하지 않고 돌아간다
+            }
+        });
+        alBuilder.setTitle("서울은 배우는중");
+        alBuilder.show(); // AlertDialog.Bulider로 만든 AlertDialog를 보여준다.
+
+
     }
 
 
