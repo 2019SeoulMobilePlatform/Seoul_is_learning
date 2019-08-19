437f005eb2efbdfae8a3c3b712dd07d515be4f02
cme10575 <cme10575@gmail.com>
2019-08-19 21:44:23 +0900

    동호회 생성시 이미지 회전 현상 수정

--- app/src/main/java/com/example/clubactivity/Club/AddClubActivity.java
+++ app/src/main/java/com/example/clubactivity/Club/AddClubActivity.java
@@ -1,10 +1,18 @@
 package com.example.clubactivity.Club;
 
 //import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -15,11 +23,16 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import com.example.clubactivity.AddClassActivity;
+import com.example.clubactivity.Constants;
+import com.example.clubactivity.ImageProcessing;
 import com.example.clubactivity.R;
 
 import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 
@@ -43,10 +56,26 @@ public class AddClubActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                intent.setType("image/*");
+                //Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                //intent.setType("image/*");
 
-                startActivityForResult(Intent.createChooser(intent, "이미지를 선택하세요"), 1000);
+                //startActivityForResult(Intent.createChooser(intent, "이미지를 선택하세요"), 1000);
+                try {
+                    if (ActivityCompat.checkSelfPermission(AddClubActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(AddClubActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, Constants.REQUEST_PICK_IMAGE);
+                    } else {
+                        Intent galleryIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                        startActivityForResult(galleryIntent, Constants.REQUEST_PICK_IMAGE);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+
+
+                //Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                //startActivityForResult(gallery, Constants.REQUEST_PICK_IMAGE);
+
             }
         });
 
@@ -120,8 +149,9 @@ public class AddClubActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if(resultCode == RESULT_OK && requestCode == 1000){
+        if(resultCode == RESULT_OK && requestCode == Constants.REQUEST_PICK_IMAGE){
             ImageView imageView = findViewById(R.id.user_image);
+            /* 회전 고치기전 코드
             try{
                 InputStream inputStream = getContentResolver().openInputStream(data.getData());
                 userImage = BitmapFactory.decodeStream(inputStream);
@@ -130,6 +160,11 @@ public class AddClubActivity extends AppCompatActivity {
             catch(FileNotFoundException e){
                 e.printStackTrace();
             }
+            */
+
+            Uri imgUri = data.getData();
+            ImageProcessing imageProcessing = new ImageProcessing(AddClubActivity.this);
+            imageProcessing.SetImage(imageView, imgUri);
         }
     }
 
